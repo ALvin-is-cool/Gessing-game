fab887295bfc99fc2b74e764d4516536f18bce8f
ALvin-is-cool <86335688+ALvin-is-cool@users.noreply.github.com>
2022-03-19 12:49:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var player1= document.getElementById("player1").value;
+var player2= document.getElementById("player2").value;
+
+function login(){
+    window.location="https://sso.prodigygame.com/game/start?rid=c5666e54-0ef6-472a-8218-1ee79f3de688";
+localStorage.setItem("player1",player1);
+localStorage.setItem("player2",player2);
+console.log(player1);
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
